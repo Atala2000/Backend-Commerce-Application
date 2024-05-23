449017016b48717abc329f0886e1c11e9cc415b0
Atala2000 <atalakidi@gmail.com>
2024-05-23 22:01:55 +0300

    Added method that returns the currently logged in user Tested that method works with Postman

--- backend/accounts/api/urls.py
+++ backend/accounts/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import RegisterUserAPIView, LoginUserAPIView, LogoutUserAPIView
+from .views import RegisterUserAPIView, LoginUserAPIView, LogoutUserAPIView, UserAPIView
 
 urlpatterns = [
     path('register/', RegisterUserAPIView.as_view(), name='register'),
     path('login/', LoginUserAPIView.as_view(), name='login'),
     path('logout/', LogoutUserAPIView.as_view(), name='logout'),
+    path('user/', UserAPIView.as_view(), name='user'),
 ]
--- backend/accounts/api/views.py
+++ backend/accounts/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from django.contrib.auth import login, logout, authenticate
 from accounts.models import CustomUser
@@ -39,4 +40,32 @@ class LogoutUserAPIView(APIView):
         logout(request)
         return Response(
             {"message": "User logged out successfully."}, status=status.HTTP_200_OK
+        )
+
+
+
+class UserAPIView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        serializer = UserSerializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request):
+        user = request.user
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {"message": "User updated successfully."}, status=status.HTTP_200_OK
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request):
+        user = request.user
+        user.delete()
+        return Response(
+            {"message": "User deleted successfully."}, status=status.HTTP_200_OK
         )
