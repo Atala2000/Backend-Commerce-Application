529ccc80ebcfa35fd4a4b83ad5ab3e431b0672c6
Atala2000 <atalakidi@gmail.com>
2024-06-01 22:35:09 +0300

    chore: Add phone field to create_user method in CustomUserManager

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -7,14 +7,14 @@ class CustomUserAdmin(UserAdmin):
     list_display = ('email', 'is_staff', 'is_active')
     list_filter = ('is_staff', 'is_active')
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
+        (None, {'fields': ('email', 'phone','password')}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'is_active', 'is_staff')}
+            'fields': ('email', 'phone','password1', 'password2', 'is_active', 'is_staff')}
         ),
     )
     search_fields = ('email',)
